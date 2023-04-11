5017b3ffc74dddc763a672b098ee46fc1f840bee
Rigoberto Calderon <jesuscalflo15@gmail.com>
2023-04-11 00:28:32 -0600

    [ADD] Se creo clase que contiene los tipos de operadores

---
+++ src/com/rcalderon/Operadores.java
@@ -0,0 +1,27 @@
+package com.rcalderon;
+
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        // Operadores aritmeticos
+        // * + - / %
+        int numberOne = 25;
+        int numberTwo = 18;
+
+        int multiply = numberOne * numberTwo;
+        int addition = numberOne + numberTwo;
+        int substract = numberOne - numberTwo;
+        int division = numberOne / numberTwo;
+        int remainder = numberOne % numberTwo;
+
+        // Operadores lógicos
+        // <= >= == < > != && || !
+
+        // Operadores de asignación
+        // =  += -= /= *= %=
+
+        // Incremento/Decremento
+        // ++ --
+    }
+}
