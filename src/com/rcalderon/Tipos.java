d1c6410b1bc3c8660dd5bc2edbd82707c0784ba2
Rigoberto Calderon <jesuscalflo15@gmail.com>
2023-04-11 00:09:44 -0600

    [ADD] Se creo clase que contiene los tipos de variables en Java

---
+++ src/com/rcalderon/Tipos.java
@@ -0,0 +1,36 @@
+package com.rcalderon;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+        // Valores enteros
+        // Tipos primitivos: int, short, long, double, float, boolean, byte, char
+        byte numberOne = 1; // 1 byte
+        short numberTwo = 2; // 2 byte
+        int numberThree = 3; // 4 byte
+        long numberFour = 4; // 8 byte
+
+        // Valores flotantes
+        float floatValue = 4.9f;
+        double deciimaltTwo =   99.99;
+
+
+        // Valor caracter
+        char character = '1';
+
+        // Valor booleanos
+        boolean isTrueOrFalse = false;
+
+        // Valores, cadenas de texto
+        String name = "R. Calderon";
+
+        // Valores de tipos envoltorios
+        Integer numberNull = null;
+
+
+
+
+
+    }
+}
